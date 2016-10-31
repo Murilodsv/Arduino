09b081763b1edf462ab03ce9964ecb66d5bcf2e9
Murilodsv <murilodsv@usp.br>
2016-10-31 16:26:56 -0200

    .ino added

---
+++ Aula_31_10.ino/Aula_31_10.ino.ino
@@ -0,0 +1,34 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  //Caso atribua valor decimal em variavel inteira, o valor será truncado
+
+boolean valorX;
+boolean valorY;
+boolean valorZ;
+
+Serial.begin(9600); // Boud-Rate para porta COM (1200 - 115200) Taxas baixas para distancia maiores e vice-versa.
+
+Serial.print("Grupo de Pesquisa Arduino - ESALQ/USP"); // usado para "printar" valores
+
+valorX = 1;
+valorY = 0;
+valorZ = (valorX && valorY); // "&&" = E() do excel "||" = OU() do excel
+
+Serial.print("\tValorX = "); //"\t" é para ajustar com um "tab" entre valores
+Serial.print(valorX,BIN); // "BIN" para mostrar o valor binario da variavel
+
+Serial.print("\tValorY = "); //"\t" é para ajustar com um "tab" entre valores
+Serial.print(valorY,BIN);
+
+Serial.print("\tValorZ = "); //"\t" é para ajustar com um "tab" entre valores
+Serial.print(valorZ,BIN);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  
+
+}
